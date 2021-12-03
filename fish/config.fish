0218647ce0b0a990c1e5c62dc20c3fd0d0aa0549
Ben Mayoras <ben.mayoras@gmail.com>
2021-12-03 10:30:04 -0500

    Add show-upstream to git prompt

--- fish/config.fish
+++ fish/config.fish
@@ -57,7 +57,7 @@ end
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate ''
-set __fish_git_prompt_showupstream 'none'
+set __fish_git_prompt_showupstream 'auto'
 set -g fish_prompt_pwd_dir_length 3
 
 function fish_prompt
